d9644ab3023985e9859524948e642bec2a63ce4f
David Florness <edwargix@gmail.com>
2016-12-30 14:20:08 -0600

    Changed default buffer

--- init.el
+++ init.el
@@ -109,6 +109,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(initial-buffer-choice "~/.emacs.d/init.el")
  '(package-selected-packages
    (quote
     (projectile js-import android-mode auctex company-tern company))))
