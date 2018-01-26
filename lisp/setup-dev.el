f54cc78b237222ac01b7409ff3102b6f5873700d
David Florness <edwargix@gmail.com>
2018-01-25 17:29:18 -0700

    Install elisp-slime-nav to find elisp symbols

--- lisp/setup-dev.el
+++ lisp/setup-dev.el
@@ -127,5 +127,16 @@
     (add-to-list 'zeal-at-point-mode-alist '(python-mode . "python"))))
 
 
+(use-package elisp-slime-nav
+  :ensure t
+  :config
+  (progn
+    (dolist (map `(,emacs-lisp-mode-map
+		   ;; ,ielm-map
+		   ,lisp-interaction-mode-map))
+      (evil-define-key 'normal map (kbd "M-.") 'elisp-slime-nav-find-elisp-thing-at-point)
+      (evil-define-key 'normal map (kbd "M-,") 'pop-tag-mark))))
+
+
 (provide 'setup-dev)
 ;;; setup-dev.el ends here
