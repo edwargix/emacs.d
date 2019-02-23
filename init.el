4096e23f1af8bee315f33b5654bcd45f23f4de92
David Florness <edwargix@gmail.com>
2019-02-22 18:08:16 -0700

    Fix EOF error

--- init.el
+++ init.el
@@ -252,6 +252,7 @@
  ;; If there is more than one, they won't work right.
  '(initial-buffer-choice t)
  '(initial-scratch-message "")
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
