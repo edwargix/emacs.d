26e518959e6d8b718f8993eb01f7c60d4a829559
David Florness <edwargix@gmail.com>
2019-05-04 13:00:13 -0600

    (kbd "K") uses man not woman
    I like having the man page appear in a separate window.

--- lisp/setup-keys.el
+++ lisp/setup-keys.el
@@ -11,6 +11,8 @@
     (define-key evil-ex-map "b " #'counsel-ibuffer)
     (define-key evil-ex-map "e " #'counsel-find-file)
     (add-hook 'with-editor-mode-hook 'evil-insert-state)
+    (evil-global-set-key 'normal (kbd "K") #'man)
+    (evil-global-set-key 'motion (kbd "K") #'man)
     (evil-global-set-key 'normal (kbd "SPC") mode-specific-map)
     (evil-global-set-key 'motion (kbd "SPC") mode-specific-map)
     (evil-global-set-key 'normal (kbd "SPC u") 'universal-argument)
