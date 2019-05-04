bd180c92c42d07f7eb1782f2e5f62367aca38579
David Florness <edwargix@gmail.com>
2019-05-04 11:31:19 -0600

    Use "markdown" executable to run markdown via markdown-mode

--- init.el
+++ init.el
@@ -52,7 +52,7 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "markdown"))
 
 
 ;;; a minor mode for dealing with pairs
