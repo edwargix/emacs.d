ef0f12eedf707a2abdfcb1588727aef1b1f563b5
David Florness <edwargix@gmail.com>
2019-05-20 11:16:14 -0600

    Transparent emacs is more transparent
    This makes it easier to read windows behind emacs

--- lisp/setup-appearance.el
+++ lisp/setup-appearance.el
@@ -45,5 +45,5 @@
                     ;; Also handle undocumented (<active> <inactive>) form.
                     ((numberp (cadr alpha)) (cadr alpha)))
               100)
-         90 100))))
+         80 100))))
 (global-set-key (kbd "C-c t") 'toggle-transparency)
