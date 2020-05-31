0872eafffdfec45ce58a3643e008cfd760bd32ed
David Florness <david@florness.com>
2020-05-30 19:43:28 -0600

    Cleanup python setup file

--- lisp/python.el
+++ lisp/python.el
@@ -1,12 +1,10 @@
 (use-package pyvenv
   :commands (pyvenv-activate pyvenv-workon))
 
-
 (use-package pipenv
   :hook (python-mode . pipenv-mode)
   :custom (pipenv-with-projectile nil)
   :commands (pipenv-activate))
 
-
 (use-package python-docstring
-  :hook (python-mode . (lambda () (python-docstring-mode t))))
+  :hook (python-mode . python-docstring-mode))
