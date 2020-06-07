debc5020fbb55b5d384a8f01c4737a9535e431dc
David Florness <david@florness.com>
2020-06-07 02:13:16 -0600

    Cleanup :mode for web-mode config and open .dhtml files in web-mode

--- lisp/web.el
+++ lisp/web.el
@@ -1,17 +1,18 @@
 (use-package web-mode
-  :mode (("\\.phtml\\'" . web-mode)
-         ("\\.tpl\\.php\\'" . web-mode)
-         ("\\.[agj]sp\\'" . web-mode)
-         ("\\.as[cp]x\\'" . web-mode)
-         ("\\.erb\\'" . web-mode)
-         ("\\.mustache\\'" . web-mode)
-         ("\\.djhtml\\'" . web-mode)
-         ("\\.html?\\'" . web-mode)
-         ("\\.xhtml\\'" . web-mode)
-         ("\\.js\\'" . web-mode)
-         ("\\.ts\\'" . web-mode)
-         ("\\.tsx\\'" . web-mode)
-         ("\\.jsx\\'" . web-mode))
+  :mode ("\\.[agj]sp\\'"
+         "\\.as[cp]x\\'"
+         "\\.dhtml\\'"
+         "\\.djhtml\\'"
+         "\\.erb\\'"
+         "\\.html?\\'"
+         "\\.js\\'"
+         "\\.jsx\\'"
+         "\\.mustache\\'"
+         "\\.phtml\\'"
+         "\\.tpl\\.php\\'"
+         "\\.ts\\'"
+         "\\.tsx\\'"
+         "\\.xhtml\\'")
   :custom
   (web-mode-content-types-alist '(("jsx"  . ".*\\.js[x]?\\'")))
   (web-mode-markup-indent-offset 2)
