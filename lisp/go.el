76f53e6ad06fe203099ebe423be80f0f35ca8d37
David Florness <david@florness.com>
2021-05-23 11:08:16 -0400

    Allow directories with a go.mod file to be a project
    This is needed to use lsp-mode with gopls when inside the GOPATH.

--- lisp/go.el
+++ lisp/go.el
@@ -1,3 +1,14 @@
 (use-package go-mode
   :defer t
   :hook (go-mode . lsp))
+
+(require 'project)
+
+(defun project-find-go-module (dir)
+  (when-let ((root (locate-dominating-file dir "go.mod")))
+    (cons 'go-module root)))
+
+(cl-defmethod project-root ((project (head go-module)))
+  (cdr project))
+
+(add-hook 'project-find-functions #'project-find-go-module)
