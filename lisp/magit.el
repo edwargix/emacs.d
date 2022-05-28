1cfa25a2cf235ad03b84aeab4965ec47dbd27ad4
David Florness <david@florness.com>
2022-05-28 16:33:01 -0400

    Move magit configuration to its own file

---
+++ lisp/magit.el
@@ -0,0 +1,7 @@
+;; magit: a Git Porcelain
+(use-package magit
+  :bind
+  (("C-x g" . magit-status)
+   ("C-x M-g" . magit-dispatch)
+   ("C-c g b" . magit-blame)
+   ("C-c g l" . magit-log)))
--- lisp/misc.el
+++ lisp/misc.el
@@ -50,14 +50,6 @@
          ("\\.markdown\\'" . markdown-mode))
   :custom (markdown-command "markdown"))
 
-;; magit: a Git Porcelain
-(use-package magit
-  :bind
-  (("C-x g" . magit-status)
-   ("C-x M-g" . magit-dispatch)
-   ("C-c g b" . magit-blame)
-   ("C-c g l" . magit-log)))
-
 ;; Syntax/error checking
 (use-package flycheck
   :config
