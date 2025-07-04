cb0c963f3ae436c7d9d77ce4a74f88eba25c5600
David Florness <david@florness.com>
2025-07-04 16:33:48 -0400

    gptel: configure ollama

--- lisp/llm.el
+++ lisp/llm.el
@@ -1,1 +1,9 @@
-(use-package gptel)
+(use-package gptel
+  :config
+  (setq
+   gptel-model 'llama3:latest
+   gptel-backend (gptel-make-ollama "Ollama"             ;Any name of your choosing
+                   :host "localhost:11434"               ;Where it's running
+                   :stream t                             ;Stream responses
+                   :models '(llama3:latest))             ;List of models
+   ))
