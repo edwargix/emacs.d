0321de86f0fe274cb17c92d8de0335634919cc50
David Florness <david@florness.com>
2026-05-09 11:44:27 -0400

    enable all doom themes

--- lisp/themes.el
+++ lisp/themes.el
@@ -37,9 +37,80 @@
 
 (use-theme color-theme-modern (pierson))
 
-(use-theme doom-themes (doom-one
+
+(use-theme doom-themes (doom-1337
+                        doom-acario-dark
+                        doom-acario-light
+                        doom-ayu-dark
+                        doom-ayu-light
+                        doom-ayu-mirage
+                        doom-badger
+                        doom-challenger-deep
+                        doom-city-lights
+                        doom-dark+
+                        doom-dracula
+                        doom-earl-grey
+                        doom-ephemeral
+                        doom-fairy-floss
+                        doom-feather-dark
+                        doom-feather-light
+                        doom-flatwhite
+                        doom-gruvbox
+                        doom-gruvbox-light
+                        doom-henna
+                        doom-homage-black
+                        doom-homage-white
+                        doom-horizon
+                        doom-Iosvkem
+                        doom-ir-black
+                        doom-lantern
+                        doom-laserwave
+                        doom-manegarm
+                        doom-material
+                        doom-material-dark
+                        doom-meltbus
+                        doom-miramare
+                        doom-molokai
+                        doom-monokai-classic
+                        doom-monokai-machine
+                        doom-monokai-octagon
+                        doom-monokai-pro
+                        doom-monokai-ristretto
+                        doom-monokai-spectrum
+                        doom-moonlight
+                        doom-nord
+                        doom-nord-aurora
+                        doom-nord-light
+                        doom-nova
+                        doom-oceanic-next
+                        doom-oksolar-dark
+                        doom-oksolar-light
+                        doom-old-hope
+                        doom-one
                         doom-one-light
-                        doom-vibrant))
+                        doom-opera
+                        doom-opera-light
+                        doom-outrun-electric
+                        doom-palenight
+                        doom-peacock
+                        doom-pine
+                        doom-plain
+                        doom-plain-dark
+                        doom-rouge
+                        doom-shades-of-purple
+                        doom-snazzy
+                        doom-solarized-dark
+                        doom-solarized-dark-high-contrast
+                        doom-solarized-light
+                        doom-sourcerer
+                        doom-spacegrey
+                        doom-tokyo-night
+                        doom-tomorrow-day
+                        doom-tomorrow-night
+                        doom-vibrant
+                        doom-wilmersdorf
+                        doom-xcode
+                        doom-zenburn))
 
 (use-theme gruvbox-theme (gruvbox-dark-hard
                           gruvbox-light-hard))
